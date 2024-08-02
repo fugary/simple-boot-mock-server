40294370bd3ea024370ccecde194e91e68aa6d22
Gary Fu <xuebaiyouling@gmail.com>
2024-08-02 21:41:52 +0800

    mysql语句错误问题

--- simple-boot-mock-server/src/main/resources/db/mysql/migration/V2_0_14__update description definition.sql
+++ simple-boot-mock-server/src/main/resources/db/mysql/migration/V2_0_14__update description definition.sql
@@ -1,3 +1,3 @@
-alter table t_mock_group alter column description text;
-alter table t_mock_request alter column description text;
-alter table t_mock_data alter column description text;
+alter table t_mock_group MODIFY column description text;
+alter table t_mock_request MODIFY column description text;
+alter table t_mock_data MODIFY column description text;
