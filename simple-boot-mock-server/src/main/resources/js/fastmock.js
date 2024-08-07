1bfd8009ba294b288d90a6d6b87f48c04161b400
Gary Fu <xuebaiyouling@gmail.com>
2024-08-07 23:27:27 +0800

    request报错处理

--- simple-boot-mock-server/src/main/resources/js/fastmock.js
+++ simple-boot-mock-server/src/main/resources/js/fastmock.js
@@ -1,31 +1,32 @@
-(function (Mock) {
-    if (!Mock.__oriMock) {
-        Mock.__oriMock = Mock.mock
-        const processMockParam = mockData => {
-            if (typeof mockData === 'object') {
-                for (const argsKey in mockData) {
-                    const dataVal = mockData[argsKey]
-                    if (typeof dataVal === 'function') {
-                        mockData[argsKey] = function () {
-                            const result = dataVal.call(this, {
-                                _req: request, // fastMock
-                                request: request,
-                                Mock: Mock
-                            })
-                            if (typeof result === 'object') {
-                                processMockParam(result);
-                            }
-                            return result
+(function (Mock, request) {
+    const processMockParam = mockData => {
+        if (typeof mockData === 'object') {
+            const argObj = {
+                _req: request, // fastMock
+                request: request,
+                Mock: Mock
+            }
+            for (const argsKey in mockData) {
+                const dataVal = mockData[argsKey]
+                if (typeof dataVal === 'function') {
+                    mockData[argsKey] = function () {
+                        const result = dataVal.call(this, argObj)
+                        if (typeof result === 'object') {
+                            processMockParam(result);
                         }
-                    } else if (typeof dataVal === 'object') {
-                        processMockParam(dataVal)
+                        return result
                     }
+                } else if (typeof dataVal === 'object') {
+                    processMockParam(dataVal)
                 }
             }
         }
-        Mock.mock = function (mockData, ...args) {
-            processMockParam(mockData)
-            return Mock.__oriMock(mockData, ...args)
-        };
     }
-})(Mock)
+    if (!Mock.__oriMock) {
+        Mock.__oriMock = Mock.mock
+    }
+    Mock.mock = function (mockData, ...args) {
+        processMockParam(mockData)
+        return Mock.__oriMock(mockData, ...args)
+    };
+})(Mock, request)
