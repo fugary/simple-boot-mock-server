13ee8506f8a854641b5e81bb7d2f30b48b903965
gary.fu <xuebaiyouling@gmail.com>
2024-08-07 10:56:54 +0800

    优化显示，优化代码提示

--- simple-boot-mock-newui/src/vendors/mockjs/MockJsHints.js
+++ simple-boot-mock-newui/src/vendors/mockjs/MockJsHints.js
@@ -1,14 +1,18 @@
 import * as monaco from 'monaco-editor'
-import BetterMockJsCode from '@/vendors/mockjs/BetterMockJs.d.ts?raw'
+import BetterMockJsCode from 'better-mock/typings/index.d.ts?raw'
 import RequestHintDataCode from '@/vendors/mockjs/RequestHintData.d.ts?raw'
+import DayjsCode from 'dayjs/index.d.ts?raw'
 import { getMockJsPlaceholders } from '@/vendors/mockjs/MockJsonHintData'
 import { initXmlWithJs } from '@/vendors/mockjs/XmlWithJs'
 
 export const initMockJsHints = () => {
   if (!monaco.languages.__initedMockJsHints__) {
     monaco.languages.__initedMockJsHints__ = true
-    monaco.languages.typescript.javascriptDefaults.addExtraLib(BetterMockJsCode, 'BetterMockJs.js')
+    const MockJsCode = BetterMockJsCode.replace(/export\s*=\s*Mock;/, 'const Mock: Mock.BetterMock;')
+      .replace('country(', 'county(') // 单词似乎拼错了
+    monaco.languages.typescript.javascriptDefaults.addExtraLib(MockJsCode, 'BetterMockJs.js')
     monaco.languages.typescript.javascriptDefaults.addExtraLib(RequestHintDataCode, 'RequestHintData.js')
+    monaco.languages.typescript.javascriptDefaults.addExtraLib(DayjsCode.replace(/export\s*=\s*dayjs;/, ''), 'DayjsCode.ts')
     monaco.languages.registerCompletionItemProvider('json', {
       triggerCharacters: ['"'],
       provideCompletionItems: getMockJsPlaceholders()
