8d8c11a68423d463b4a3126e9075b9bf7041b0f1
gary.fu <xuebaiyouling@gmail.com>
2025-04-30 11:01:44 +0800

    feat: request列表页面增加分页查询，优化展示效果

--- simple-boot-mock-server/src/main/java/com/fugary/simple/mock/entity/mock/CountData.java
+++ simple-boot-mock-server/src/main/java/com/fugary/simple/mock/entity/mock/CountData.java
@@ -1,6 +1,7 @@
 package com.fugary.simple.mock.entity.mock;
 
 import lombok.Data;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -22,9 +23,9 @@ public class CountData implements Serializable {
     }
 
     public CountData(Map<String, Object> countMap) {
-        Object groupKeyObj = countMap.get("GROUP_KEY");
+        Object groupKeyObj = ObjectUtils.defaultIfNull(countMap.get("GROUP_KEY"), countMap.get("group_key"));
         this.groupKey = Objects.toString(groupKeyObj);
-        Object dataCountObj = countMap.get("DATA_COUNT");
+        Object dataCountObj = ObjectUtils.defaultIfNull(countMap.get("DATA_COUNT"), countMap.get("data_count"));
         if (dataCountObj instanceof Long) {
             this.dataCount = (Long) dataCountObj;
         }
