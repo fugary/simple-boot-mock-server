ceb1552bcaea1c3e4069c6bca19082bf56e5ec5f
gary.fu <xuebaiyouling@gmail.com>
2025-05-30 14:46:43 +0800

    bug: 去掉一些粘贴进去处理逻辑，减少格式问题

--- simple-boot-mock-newui/src/vendors/monaco-editor.js
+++ simple-boot-mock-newui/src/vendors/monaco-editor.js
@@ -21,7 +21,9 @@ const defaultConfig = {
   wordWrap: 'on',
   readOnly: true,
   language: 'javascript',
-  fixedOverflowWidgets: true
+  fixedOverflowWidgets: true,
+  formatOnPaste: true,
+  internalPasteProcess: false
 }
 
 /**
@@ -166,7 +168,7 @@ export const useMonacoEditorOptions = (config) => {
     if (!oldContent && content) {
       checkEditorLang()
     }
-    if (editorRef.value && !editorRef.value.__internalPasteFunc__) {
+    if (editorRef.value && !editorRef.value.__internalPasteFunc__ && monacoEditorOptions.internalPasteProcess) {
       const editor = toRaw(editorRef.value)
       editor.__internalPasteFunc__ = () => {
         const value = editor.getValue()
