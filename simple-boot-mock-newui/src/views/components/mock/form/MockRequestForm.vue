153616fa5d62b7b877c675f619f46a16762ebf65
gary.fu <xuebaiyouling@gmail.com>
2025-11-05 17:50:00 +0800

    feat: 增加请求参数列表复制功能，可以方便复制到其他请求中使用

--- simple-boot-mock-newui/src/views/components/mock/form/MockRequestForm.vue
+++ simple-boot-mock-newui/src/views/components/mock/form/MockRequestForm.vue
@@ -48,7 +48,7 @@ const requestUrl = computed(() => {
   let reqUrl = props.requestPath
   paramTarget.value?.pathParams?.forEach(pathParam => {
     reqUrl = reqUrl.replace(new RegExp(`:${pathParam.name}`, 'g'), pathParam.value)
-      .replace(new RegExp(`\\{${pathParam.name}\\}`, 'g'), pathParam.value)
+      .replace(new RegExp(`\\{${pathParam.name}\\}`, 'g'), processEvnParams(paramTarget.value.groupConfig, pathParam.value, true))
   })
   if (paramTarget.value?.method?.toLowerCase() === 'get') {
     const calcReqParams = paramTarget.value?.requestParams?.filter(requestParam => !!requestParam.name && requestParam.enabled).reduce((results, item) => {
